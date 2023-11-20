ee27f068d7ec3f6102f06c06ea14346f66dbcd2c
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-20 14:26:22 -0500

    Built contact page

--- React Portfolio/src/pages/Contact.jsx
+++ React Portfolio/src/pages/Contact.jsx
@@ -1,5 +1,84 @@
+import { useState } from "react";
+import { validateEmail } from "../utils/helpers";
+
 export default function Contact() {
-    return (
-        <h2>CONTACT</h2>
-    )
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setErrorMessage("Please enter a valid email.");
+      return;
+    }
+    alert(`Thank you for submitting your message!`);
+    setName("");
+    setMessage("");
+    setEmail("");
+  };
+
+  return (
+    <section className="d-flex">
+      <div className="mt-5 col-xl-4 col-md-6 col-12 border container text-center">
+        <h1>CONTACT ME:</h1>
+        <form className="form d-flex flex-column" onSubmit={handleFormSubmit}>
+          <input
+            className="my-3"
+            value={email}
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={() => {
+              if (!email) {
+                setErrorMessage("Email is required");
+              } else {
+                setErrorMessage("");
+              }
+            }}
+            type="email"
+            placeholder="email"
+          />
+          <input
+            className="my-3"
+            value={name}
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            onBlur={() => {
+              if (!name) {
+                setErrorMessage("Name is required");
+              } else {
+                setErrorMessage("");
+              }
+            }}
+            type="text"
+            placeholder="name"
+          />
+          <textarea
+            className="my-3"
+            value={message}
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+            onBlur={() => {
+              if (!message) {
+                setErrorMessage("Message is required");
+              } else {
+                setErrorMessage("");
+              }
+            }}
+            type="text"
+            placeholder="message"
+          />
+          <button className="btn btn-dark" type="submit">
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
