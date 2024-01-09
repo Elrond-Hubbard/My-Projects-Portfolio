85544b9c00271aee463e08af40a2b676e3cbf3cb
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2024-01-09 17:21:24 -0500

    updated resume

--- src/components/resumeCard.jsx
+++ src/components/resumeCard.jsx
@@ -27,10 +27,8 @@ export default function ResumeCard() {
         <h4>Full Stack Web Development {`(Certificate)`}</h4>
         <p>University of Central Florida | Orlando, FL | 2023</p>
 
-        <h4>Film and Digital Media {`(A.A.)`}</h4>
-        <p>University of North Georgia | Athens, GA | 2011</p>
         <h1>Work Experience</h1>
-        <h4>541 Beville LLC</h4>
+        <h4>C's Family Diner</h4>
         <h4>2020-Present</h4>
         <p>
           Supervised staff, managed inventory, and executed company-wide
@@ -38,16 +36,14 @@ export default function ResumeCard() {
         </p>
         <ul>
           <li>
-            Reconfigured POS systems at 4 locations to improve ticket times by
-            18%
+          Configured POS systems at 4 locations to improve ticket times and staff efficiency.
           </li>
           <li>
             Coordinated with food distributors to ensure accurate inventory
             deliveries in a timely manner.
           </li>
-          <li>Promoted to manager after 6 months. </li>
           <li>
-            Onboarded 8 new candidates and created training guides to improve
+            Onboarded new candidates and created training guides to improve
             employee performance.
           </li>
         </ul>
